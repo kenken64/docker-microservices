ff9226212e7028a1af7c2c8df64e615f8d28cb24
Ubuntu <ubuntu@ip-172-31-17-171.ap-southeast-1.compute.internal>
2019-09-30 15:51:22 +0000

    upd

--- server/server.js
+++ server/server.js
@@ -1,1 +1,9 @@
 console.log("test");
+const express = require("express")
+app = express()
+port = process.env.PORT || 3000;
+
+
+app.listen(port, ()=>{
+	console.log(`Listening on port => ${port}`
+});
