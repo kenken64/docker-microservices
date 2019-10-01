5e0561148dcacdc6ea16d8e1687085b7481b5244
Ubuntu <ubuntu@ip-172-31-17-171.ap-southeast-1.compute.internal>
2019-10-01 08:08:57 +0000

    upd

--- database/sql-scripts/data.sql
+++
@@ -1,8 +0,0 @@
-TRUNCATE TABLE tasks;
-
-INSERT INTO `tasks` (`id`, `task`, `status`, `created_at`) VALUES
-(1, 'Find bugs', 1, '2016-04-10 23:50:40'),
-(2, 'Review code', 1, '2016-04-10 23:50:40'),
-(3, 'Fix bugs', 1, '2016-04-10 23:50:40'),
-(4, 'Refactor Code', 1, '2016-04-10 23:50:40'),
-(5, 'Push to prod', 1, '2016-04-10 23:50:50');
---
+++ database/sql-scripts/database.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS tasks;
+
+CREATE TABLE IF NOT EXISTS tasks (
+  id int(11) NOT NULL,
+  task varchar(200) NOT NULL,
+  status tinyint(1) NOT NULL DEFAULT 1,
+  created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+ 
+ALTER TABLE tasks ADD PRIMARY KEY (id);
+ALTER TABLE tasks MODIFY id int(11) NOT NULL AUTO_INCREMENT;
+
+INSERT INTO tasks (id, task, status, created_at) VALUES
+(1, 'Find bugs', 1, '2016-04-10 23:50:40'),
+(2, 'Review code', 1, '2016-04-10 23:50:40'),
+(3, 'Fix bugs', 1, '2016-04-10 23:50:40'),
+(4, 'Refactor Code', 1, '2016-04-10 23:50:40'),
+(5, 'Push to prod', 1, '2016-04-10 23:50:50');
