da59f26e1c55cf3695141d049fdbb9cfca4549bd
Kenneth Phang <bunnyppl@gmail.com>
2019-10-03 02:58:45 +0800

    sql

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from './app.service';
+import { MatTableDataSource } from '@angular/material';
+
+export interface Tasks {
+  id: String;
+  task: String;
+  status: Number;
+  created_at: Number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,16 +16,23 @@ import { AppService } from './app.service';
 })
 export class AppComponent implements OnInit{
   title = 'angular-app';
-  tasks: any = [];
+  dataSource: MatTableDataSource<Tasks>;
+  displayedColumns: string[] = ['id', 'task', 'status','created_at'];
+
   constructor(private svc: AppService){
 
   }
 
   ngOnInit(){
     console.log("page init loaded...");
+    
     this.svc.getAllTasks().then(result=>{
-      console.log(result)
-      this.tasks = result;
-    })
+      console.log(result);
+      let arr= [];
+      result.forEach((data)=>{
+        arr.push({id: data[0], task: data[1], status: data[2], created_at: data[3] })
+      })
+      this.dataSource = new MatTableDataSource(arr);
+    });
   }
 }
--- database/sql-scripts/database.sql
+++ database/sql-scripts/database.sql
@@ -16,3 +16,21 @@ INSERT INTO tasks (id, task, status, created_at) VALUES
 (3, 'Fix bugs', 1, '2016-04-10 23:50:40'),
 (4, 'Refactor Code', 1, '2016-04-10 23:50:40'),
 (5, 'Push to prod', 1, '2016-04-10 23:50:50');
+
+CREATE TABLE IF NOT EXISTS friends (
+  id int(11) NOT NULL,
+  name varchar(200) NOT NULL,
+  contactNo varchar(200) NOT NULL,
+  email varchar(200) NOT NULL,
+  created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+ 
+ALTER TABLE friends ADD PRIMARY KEY (id);
+ALTER TABLE friends MODIFY id int(11) NOT NULL AUTO_INCREMENT;
+
+INSERT INTO friends (id, contactNo, email, created_at) VALUES
+(1, 'Alexandra Mathew', '97734343', 'a@a.com', '2016-04-10 23:50:40'),
+(2, 'Alex Chia', '97734343', 'a@a.com', '2016-04-10 23:50:40'),
+(3, 'Sivakumar', '97734343', 'a@a.com', '2016-04-10 23:50:40'),
+(4, 'Lim Beng', '97734343', 'a@a.com', '2016-04-10 23:50:40'),
+(5, 'Ahmad Binte Alliyah', '97734343', 'a@a.com', '2016-04-10 23:50:50');
