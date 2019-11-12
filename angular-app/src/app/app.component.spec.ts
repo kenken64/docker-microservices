3c3e4f517541be98a3e7d51f0f8d530f26ec60e5
kenken64 <bunnyppl@hotmail.com>
2019-11-12 23:21:02 +0800

    upd

--- angular-app/src/app/app.component.spec.ts
+++ angular-app/src/app/app.component.spec.ts
@@ -22,10 +22,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('angular-app');
   });
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-app!');
-  });
 });
