03db80bd485180d4c86a4d788d00f3d9b492510c
Ubuntu <ubuntu@ip-172-31-17-171.ap-southeast-1.compute.internal>
2019-11-18 03:34:46 +0000

    upd

--- angular-app/src/environments/environment.prod.ts
+++ angular-app/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   api_url: 'http://ec2-13-229-233-153.ap-southeast-1.compute.amazonaws.com:8080/api/',
   friends_api_url: 'http://ec2-13-229-233-153.ap-southeast-1.compute.amazonaws.com:8081/friends',
-  artists_api_url: 'http://ec2-13-229-233-153.ap-southeast-1.compute.amazonaws.com:5000/artists'
+  artists_api_url: 'http://ec2-13-229-233-153.ap-southeast-1.compute.amazonaws.com:5001/artists'
 };
